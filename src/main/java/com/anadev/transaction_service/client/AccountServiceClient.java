bb3fc6e396f153a4cd9b29c859c164d8167c8499
Ana Carolina Cordeiro Aguilar <carolana.c40@gmail.com>
2025-12-26 18:26:50 -0300

    feat: mudando de maquina e commitando ultima atualizacao

--- src/main/java/com/anadev/transaction_service/client/AccountServiceClient.java
+++ src/main/java/com/anadev/transaction_service/client/AccountServiceClient.java
@@ -4,6 +4,7 @@ import com.anadev.transaction_service.client.dto.AccountDTO;
 import com.anadev.transaction_service.client.dto.UserDTO;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 @Service
 public class AccountServiceClient {
@@ -14,43 +15,30 @@ public class AccountServiceClient {
         this.webClient = webClientBuilder.baseUrl("http://localhost:8080").build();
     }
 
-    public AccountDTO getAccountById(Long accountId) {
+    public Mono<AccountDTO> getAccountById(Long userId, Long accountId) {
         return webClient.get()
-                .uri("/users/{idUser}/accounts/{idAccount}", accountId)
+                .uri("/users/{idUser}/accounts/{idAccount}", userId, accountId)
                 .retrieve()
-                .bodyToMono(AccountDTO.class)
-                .block();
+                .bodyToMono(AccountDTO.class);
     }
 
-    public UserDTO getUserById(Long userId){
-        return webClient.get()
-                .uri("/users/{idUser}", userId)
-                .retrieve()
-                .bodyToMono(UserDTO.class)
-                .block();
+    public Mono<AccountDTO> getMonthlyLimit(Long userId, Long accountId){
+        Mono<AccountDTO> account = getAccountById(userId,accountId);
+        webClient.get()
+                .uri()
 
     }
 
-    public Double getMonthlyLimit(Long accountId){
-       AccountDTO account = getAccountById(accountId);
-
-       return account.monthlyLimit();
-    }
-
-    public Double getCurrencyBalance(Long accountId){
-        AccountDTO account = getAccountById(accountId);
-        return account.currentBalance();
-    }
-
-    public void setCurrencyBalance(Long accountId, Double value){
+    public void setCurrencyBalance(Long idUser, Long accountId, Double value){
         webClient.patch()
-                .uri("/{idAccount}/limit", accountId)
+                .uri("/users/{idUser}/accounts/{idAccount}/balance", idUser ,accountId)
                 .bodyValue(value)
                 .retrieve()
-                .bodyToMono(AccountDTO.class)
-                .block();
+                .bodyToMono(AccountDTO.class);
 
     }
+
+
 
 
 
