57cbac154963717cc810f98f1fd5cf36ffcdf08b
Ana Carolina Cordeiro Aguilar <ana.aguilar@agi.com.br>
2026-01-09 17:56:44 -0300

    feat: incio do controller de transaction

--- src/main/java/com/anadev/transaction_service/controller/TransactionController.java
+++ src/main/java/com/anadev/transaction_service/controller/TransactionController.java
@@ -2,12 +2,21 @@ package com.anadev.transaction_service.controller;
 
 import com.anadev.transaction_service.database.DTO.TransactionRequest;
 import com.anadev.transaction_service.database.DTO.TransactionResponse;
+import com.anadev.transaction_service.database.collection.enums.TypeCategory;
+import com.anadev.transaction_service.database.collection.enums.TypeTransaction;
 import com.anadev.transaction_service.service.TransactionService;
 import lombok.RequiredArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @RestController
 @RequestMapping("/transactions")
@@ -17,6 +26,39 @@ public class TransactionController {
 
     @PostMapping
     public TransactionResponse saveTransaction(@RequestBody TransactionRequest data){
-        return transactionService.saveTransaction(data);
+        return transactionService.createTransaction(data);
     }
+
+    @GetMapping
+    public List<TransactionResponse> getAllTransactions(){
+
+        return transactionService.getAllTransactions();
+    }
+
+    @GetMapping("/{id}")
+    public TransactionResponse getTransactionById(ObjectId id){
+        return transactionService.getTransactionById(id);
+    }
+
+    @GetMapping("/{idUser}")
+    public List<TransactionResponse> getAllTransactionsByUser(@PathVariable Long idUser){
+        return transactionService.getAllTransactionsByUser(idUser);
+    }
+
+    @GetMapping("/{idUser}/category")
+    public List<TransactionResponse> getAllTransactionsByUserAndCategory(@PathVariable Long idUser,
+                                                                         TypeCategory category){
+        return transactionService.getTransactionsByUserAndCategory(idUser,category);
+    }
+
+    @GetMapping("/{idUser}/type")
+    public List<TransactionResponse> getAllByUserAndType(@PathVariable Long idUser, TypeTransaction typeTransaction){
+
+        return transactionService.getTransactionsByUserAndType(idUser,typeTransaction);
+    }
+
+    @GetMapping("/{idUser}/date")
+    public List<TransactionResponse> getAllByUserAndDate(@PathVariable Long idUser, LocalDateTime occurredAt)
+
+
 }
