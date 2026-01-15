ef0aae4cc6d1b308476b9af0fcda847415a39605
Ana Carolina Cordeiro Aguilar <carolana.c40@gmail.com>
2026-01-15 10:29:45 -0300

    fix: criacao de cosntrutor private nos mappers

--- src/main/java/com/anadev/transaction_service/mapper/TotalInputsMapper.java
+++ src/main/java/com/anadev/transaction_service/mapper/TotalInputsMapper.java
@@ -1,14 +1,12 @@
 package com.anadev.transaction_service.mapper;
 
 import com.anadev.transaction_service.database.DTO.TotalInputsResponse;
-import com.anadev.transaction_service.service.TransactionService;
 
 import java.math.BigDecimal;
 
 public final class TotalInputsMapper {
 
-    private TransactionService transactionService;
-
+    private TotalInputsMapper(){}
 
     public static TotalInputsResponse toInputsResponse(BigDecimal totalInputs){
 
--- src/main/java/com/anadev/transaction_service/mapper/TotalOutputsMapper.java
+++ src/main/java/com/anadev/transaction_service/mapper/TotalOutputsMapper.java
@@ -1,13 +1,12 @@
 package com.anadev.transaction_service.mapper;
 
 import com.anadev.transaction_service.database.DTO.TotalOutputsResponse;
-import com.anadev.transaction_service.service.TransactionService;
 
 import java.math.BigDecimal;
 
 public final class TotalOutputsMapper {
 
-     private TransactionService transactionService;
+    private TotalOutputsMapper(){}
 
      public static TotalOutputsResponse toOutputResponse(BigDecimal outputs){
          return TotalOutputsResponse.builder()
