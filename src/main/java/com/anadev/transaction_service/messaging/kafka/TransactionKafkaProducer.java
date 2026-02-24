7ddba27c29bd8acacf8da1cc1fa57cba6ced10db
Ana Carolina Cordeiro Aguilar <carolana.c40@gmail.com>
2026-02-24 16:23:18 -0300

    retira import nao utilizado

--- src/main/java/com/anadev/transaction_service/messaging/kafka/TransactionKafkaProducer.java
+++ src/main/java/com/anadev/transaction_service/messaging/kafka/TransactionKafkaProducer.java
@@ -6,7 +6,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.KafkaTemplate;
 
-import java.math.BigDecimal;
 
 @Configuration
 public class TransactionKafkaProducer {
